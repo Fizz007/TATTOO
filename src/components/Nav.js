3e25c68bc8f6cd39a2e892e7d0e4c6e50792f8ca
Faisal Abrar <erfaisalabrar@gmail.com>
2023-02-26 23:53:06 +0530

    Tattoo

---
+++ src/components/Nav.js
@@ -0,0 +1,28 @@
+import React from 'react';
+// import nav data
+import { navData } from '../data';
+
+const Nav = () => {
+  // destructure nav data
+  const { items } = navData;
+  return (
+    <nav>
+      <ul className='flex gap-x-[58px]'>
+        {items.map((item, index) => {
+          return (
+            <li key={index}>
+              <a
+                className='link hover:border-b-2 hover:border-dark transition duration-300'
+                href={item.href}
+              >
+                {item.name}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
